a0de452c01408ffb67d575ba55da321c5e106191
Paul-Willy Jean <paulwillyjean@paulwillyjean.tech>
2023-03-28 13:03:56 -0400

    Removes redundant app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +18,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent,
     MainNavComponent,
     HomeComponent,
   ],
@@ -39,6 +37,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainNavComponent]
 })
 export class AppModule { }
